e3aea5f8f0a778c6c561a24788bf059d0327d47a
gabrielaraujoz <garaujo.eng@gmail.com>
2021-01-24 10:42:54 -0300

    Primeiro commit com splash screen

---
+++ app/src/main/java/com/gabrielaraujoz/firebasechallenge/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gabrielaraujoz.firebasechallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/gabrielaraujoz/firebasechallenge/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.gabrielaraujoz.firebasechallenge.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gabrielaraujoz.firebasechallenge.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
